69c0117b4dff8acdbab2dfe138277d024271bf24
Zakir Hossen <web.zakirbd@gmail.com>
2020-07-28 09:37:26 +0600

    web.php route file update

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/{any?}', function () {
     return view('app');
 })->where('any', '[\/\w\.-]*');
 
-// Route::get('/{vue_capture?}', function () {
-//     return view('app');
-// })->where('vue_capture', '[\/\w\.-]*');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
