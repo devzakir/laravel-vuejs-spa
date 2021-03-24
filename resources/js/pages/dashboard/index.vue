c5a0e33ad78ee94d205ef2c7465655706776a258
Zakir Hossen <web.zakirbd@gmail.com>
2021-03-24 21:00:05 +0600

    dashboard page add

---
+++ resources/js/pages/dashboard/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+      <div class="card">
+        <div class="card-header">
+            Dashboard
+        </div>
+        <div class="card-body">
+            Welcome, {{ user.name }}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        user() {
+            return this.$store.getters.getUser;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
